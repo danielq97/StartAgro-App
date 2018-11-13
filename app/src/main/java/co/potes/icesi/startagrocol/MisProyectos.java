e0a04c10cece6f3fd4056535d3a8301298841a47
danielq97 <discover_daniel@hotmail.com>
2018-11-12 22:11:33 -0500

    Probando mis proyectos

--- app/src/main/java/co/potes/icesi/startagrocol/MisProyectos.java
+++ app/src/main/java/co/potes/icesi/startagrocol/MisProyectos.java
@@ -2,12 +2,33 @@ package co.potes.icesi.startagrocol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 
 public class MisProyectos extends AppCompatActivity {
+
+   LinearLayout contenedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_proyectos);
+
+        contenedor = findViewById(R.id.contene);
+
+        for (int i=0;i <=1;i++){
+            LinearLayout nuevo = new LinearLayout(getApplicationContext());
+            nuevo.setLayoutParams(new LinearLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            nuevo.setOrientation(LinearLayout.VERTICAL);
+            Button btn = new Button(getApplicationContext());
+            btn.setText("prueba");
+
+            nuevo.addView(btn);
+
+            contenedor.addView(contenedor);
+        }
+
+
     }
 }
