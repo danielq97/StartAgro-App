f3ffb838432bb899986a79156d41f24257dbe918
CristianMoralesLopez <cristianalejandromoraleslopez@gmail.com>
2018-11-15 02:41:38 -0500

    Se crea la lista para el home y su adaptador se crea la clase proyecto

--- app/src/main/java/co/potes/icesi/startagrocol/Home.java
+++ app/src/main/java/co/potes/icesi/startagrocol/Home.java
@@ -1,13 +1,104 @@
 package co.potes.icesi.startagrocol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import co.potes.icesi.startagrocol.model.AdaptadorListaHome;
+import co.potes.icesi.startagrocol.model.Proyecto;
 
 public class Home extends AppCompatActivity {
+
+
+    private ImageButton btnBusqueda;
+    private EditText txtBusqueda;
+    private ListView lista;
+    private AdaptadorListaHome adaptadorListas;
+    private ArrayList<Proyecto> listPlaylist;
+    private FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        String applicationID = "301664";
+
+        db = FirebaseDatabase.getInstance();
+
+        DatabaseReference reference = db.getReference().child("Proyectos");
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                    String titulo =(String) postSnapshot.child("Titulo").getValue();
+                    String url = (String) postSnapshot.child("url").getValue();
+                    String descripcion = (String) postSnapshot.child("Descripcion").getValue();
+
+
+                    Proyecto proyecto = new Proyecto();
+
+                    proyecto.setDescripcion(descripcion);
+
+
+
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+        btnBusqueda = findViewById(R.id.btnBusqueda);
+        txtBusqueda = findViewById(R.id.txtBusqueda);
+        lista = findViewById(R.id.lista);
+        adaptadorListas = new AdaptadorListaHome(this);
+        lista.setAdapter(adaptadorListas);
+        listPlaylist = new ArrayList<Proyecto>();
+
+
+
+
+
+        btnBusqueda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                adaptadorListas.limpiar();
+
+                Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/co/potes/icesi/startagrocol/model/Proyecto.java
@@ -0,0 +1,36 @@
+package co.potes.icesi.startagrocol.model;
+
+public class Proyecto {
+
+    private String url;
+    private String descripcion;
+    private String titulo;
+
+
+    public Proyecto() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
