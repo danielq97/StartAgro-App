7195e689fce5ec73bfc824aa0681258162cc54b9
danielq97 <discover_daniel@hotmail.com>
2018-11-15 00:54:56 -0500

    Creo la activity publicar proyecto

---
+++ app/src/main/java/co/potes/icesi/startagrocol/Publicar.java
@@ -0,0 +1,13 @@
+package co.potes.icesi.startagrocol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Publicar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicar);
+    }
+}
